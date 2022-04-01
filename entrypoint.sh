aae904a8a48583c5346c3141a2ba17cfa0ae6bed
Nicolas Rochelemagne <rochelemagne@cpanel.net>
2022-04-01 09:50:59 -0600

    Adjust scripts for Perl 5.35

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,14 @@
 #!/bin/sh -l
 
-#echo "Hello $1"
 echo "Start container"
 
 rpm -qf /etc/redhat-release
 
-#time=$(date)
-#echo ::set-output name=time::$time
-
 echo "Runing from: "
 pwd
 
-/usr/local/cpanel/3rdparty/perl/532/bin/perl -V
+/usr/local/cpanel/3rdparty/perl/535/bin/perl -V
 
 which perl
 ls -l $(which perl)
 
-# echo "sleeping forever"
-# while true; do sleep 1000; done
-# echo "done"
